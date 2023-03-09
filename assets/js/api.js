ac5e76d77400398015a68621926ec1cb56c6ea71
Cody <cody.king04@outlook.com>
2023-03-09 01:11:48 -0500

    Added PUT property to api

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,7 +5,16 @@ const api = {
         .then(response => {
             callback(response);
         })
-        .catch((error) => {
+        .catch(error => {
+            console.log(error);
+        });
+    },
+    PUT: (documentID, data, callback) => {
+        axios.put(`${api.endpoint}${documentID}`, data)
+        .then(response => {
+            callback(response);
+        })
+        .catch(error => {
             console.log(error);
         });
     }
